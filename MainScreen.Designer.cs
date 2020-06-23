c3461e1c69b05beba10e2d86a828c0bb2e60b6b7
Kantithat Kanfung <51295343+Kantithat-Kanfung@users.noreply.github.com>
2020-06-24 01:13:56 +0700

    Add files via upload

---
+++ MainScreen.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace car_installment
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExitApp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxPrice = new System.Windows.Forms.TextBox();
+            this.tbxDown = new System.Windows.Forms.TextBox();
+            this.tbxMonth = new System.Windows.Forms.TextBox();
+            this.tbxRate = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.rbDownMoney = new System.Windows.Forms.RadioButton();
+            this.rbDownPercent = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.panel1.Controls.Add(this.btnExitApp);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExitApp
+            // 
+            this.btnExitApp.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExitApp.FlatAppearance.BorderColor = System.Drawing.Color.DarkTurquoise;
+            this.btnExitApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExitApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExitApp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExitApp.Location = new System.Drawing.Point(310, 8);
+            this.btnExitApp.Name = "btnExitApp";
+            this.btnExitApp.Size = new System.Drawing.Size(37, 39);
+            this.btnExitApp.TabIndex = 16;
+            this.btnExitApp.Text = "X";
+            this.btnExitApp.UseVisualStyleBackColor = false;
+            this.btnExitApp.Click += new System.EventHandler(this.btnExitApp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "โปรแกรมคำนวณงวดรถ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ราคา";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(13, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "เงินดาวน์";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(14, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "จำนวนงวด";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(14, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ดอกเบี้ย";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(22, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ค่างวด";
+            // 
+            // tbxPrice
+            // 
+            this.tbxPrice.BackColor = System.Drawing.Color.White;
+            this.tbxPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPrice.Location = new System.Drawing.Point(133, 71);
+            this.tbxPrice.Name = "tbxPrice";
+            this.tbxPrice.Size = new System.Drawing.Size(164, 29);
+            this.tbxPrice.TabIndex = 6;
+            // 
+            // tbxDown
+            // 
+            this.tbxDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxDown.Location = new System.Drawing.Point(133, 120);
+            this.tbxDown.Name = "tbxDown";
+            this.tbxDown.Size = new System.Drawing.Size(164, 29);
+            this.tbxDown.TabIndex = 7;
+            // 
+            // tbxMonth
+            // 
+            this.tbxMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxMonth.Location = new System.Drawing.Point(134, 212);
+            this.tbxMonth.Name = "tbxMonth";
+            this.tbxMonth.Size = new System.Drawing.Size(91, 29);
+            this.tbxMonth.TabIndex = 8;
+            // 
+            // tbxRate
+            // 
+            this.tbxRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxRate.Location = new System.Drawing.Point(134, 265);
+            this.tbxRate.Name = "tbxRate";
+            this.tbxRate.Size = new System.Drawing.Size(91, 29);
+            this.tbxRate.TabIndex = 9;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.btnCalc.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnCalc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.ForeColor = System.Drawing.Color.White;
+            this.btnCalc.Location = new System.Drawing.Point(133, 316);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(114, 44);
+            this.btnCalc.TabIndex = 10;
+            this.btnCalc.Text = "คำนวณ";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.BackColor = System.Drawing.Color.Silver;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbResult.Location = new System.Drawing.Point(133, 382);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(164, 35);
+            this.lbResult.TabIndex = 11;
+            this.lbResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rbDownMoney
+            // 
+            this.rbDownMoney.AutoSize = true;
+            this.rbDownMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDownMoney.Location = new System.Drawing.Point(134, 166);
+            this.rbDownMoney.Name = "rbDownMoney";
+            this.rbDownMoney.Size = new System.Drawing.Size(59, 28);
+            this.rbDownMoney.TabIndex = 12;
+            this.rbDownMoney.TabStop = true;
+            this.rbDownMoney.Text = "บาท";
+            this.rbDownMoney.UseVisualStyleBackColor = true;
+            // 
+            // rbDownPercent
+            // 
+            this.rbDownPercent.AutoSize = true;
+            this.rbDownPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDownPercent.Location = new System.Drawing.Point(225, 166);
+            this.rbDownPercent.Name = "rbDownPercent";
+            this.rbDownPercent.Size = new System.Drawing.Size(43, 28);
+            this.rbDownPercent.TabIndex = 13;
+            this.rbDownPercent.TabStop = true;
+            this.rbDownPercent.Text = "%";
+            this.rbDownPercent.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label8.Location = new System.Drawing.Point(231, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 24);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "(เดือน)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label9.Location = new System.Drawing.Point(231, 265);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 24);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "% ต่อปี";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnClear.Location = new System.Drawing.Point(235, 316);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(62, 44);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "ยกเลิก";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(350, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.rbDownPercent);
+            this.Controls.Add(this.rbDownMoney);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.tbxRate);
+            this.Controls.Add(this.tbxMonth);
+            this.Controls.Add(this.tbxDown);
+            this.Controls.Add(this.tbxPrice);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxPrice;
+        private System.Windows.Forms.TextBox tbxDown;
+        private System.Windows.Forms.TextBox tbxMonth;
+        private System.Windows.Forms.TextBox tbxRate;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.RadioButton rbDownMoney;
+        private System.Windows.Forms.RadioButton rbDownPercent;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnExitApp;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ MainScreen.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_installment
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (tbxPrice.Text.ToString() == "")
+            {
+                MessageBox.Show("Please input your price!");
+            }
+            else if (tbxDown.Text.ToString() == "")
+            {
+                MessageBox.Show("Please input your down payment!");
+            }
+            else if (tbxMonth.Text.ToString() == "")
+            {
+                MessageBox.Show("Please input your month!");
+            }
+            else if (tbxRate.Text.ToString() == "")
+            {
+                MessageBox.Show("Please input your rate!");
+            }
+            else
+            {
+                double price = Convert.ToDouble(tbxPrice.Text);
+                double down = 0;
+
+                if (rbDownMoney.Checked)
+                {
+                    down = Convert.ToDouble(tbxDown.Text);
+                }
+                else if (rbDownPercent.Checked)
+                {
+                    down = price * Convert.ToDouble(tbxDown.Text) / 100;
+                }
+                else
+                {
+                    down = Convert.ToDouble(tbxDown.Text);
+                }
+
+                double month = Convert.ToDouble(tbxMonth.Text);
+                double rate = Convert.ToDouble(tbxRate.Text) / 100;
+                double ratePerMonth = rate / 12;
+                double finance = price - down;
+                double interest = finance * ratePerMonth * month;
+                double total = finance + interest;
+                double installment = total / month;
+                lbResult.Text = installment.ToString("0,000,000.00");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbxPrice.Clear();
+            tbxDown.Clear();
+            tbxMonth.Clear();
+            tbxRate.Clear();
+        }
+
+        private void btnExitApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
